ca9a764d29caaf53df3bf477c4efe8a31f1e5e95
atikdev-bd <atikdev.bd@gmail.com>
2022-11-15 16:50:49 +0600

    login and register form added and get user information use react-hook-form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,79 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import GoogleIcon from "../../assets/Svg/icons8-google.svg";
 
 const Login = () => {
-    return (
-        <div>
-           <h1>this is login page</h1> 
+
+const {register, handleSubmit} = useForm()
+
+const loginInfo = (data)=>{
+  console.log(data);
+}
+
+  return (
+    <div className="hero min-h-screen ">
+      <div className="hero-content flex-col ">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+        
         </div>
-    );
+        <form onSubmit={handleSubmit(loginInfo)}>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-zinc-100">
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+              {...register('email')}
+                type="text"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+              {...register('password')}
+                type="text"
+                placeholder="password"
+                className="input input-bordered"
+              />
+              <label className="label">
+                <Link href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </Link>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-accent">Login</button>
+            </div>
+            <div>
+              <h1>
+                New to Doctors Portal?{" "}
+                <Link className="text-secondary" to="/register">
+                  Create new account
+                </Link>
+              </h1>
+            </div>
+            <div className="divider text-slate-900">OR</div>
+            <div
+            //   onClick={""}
+              id="login-id"
+              className="flex mt-4 justify-center items-center border cursor-pointer bg-emerald-200 hover:bg-emerald-300  rounded-full"
+            >
+              <img className="w-12 " src={GoogleIcon} alt="" />
+              <p className="px-4">continue with google</p>
+            </div>
+          </div>
+        </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,11 +1,91 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import GoogleIcon from "../../assets/Svg/icons8-google.svg";
 
 const Register = () => {
-    return (
-        <div>
-            This is register page
+
+    const {register , handleSubmit} = useForm()
+
+    const registerInfo = (data)=>{
+        console.log(data)
+    }
+  return (
+    <div>
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Register Here !</h1>
+           
+          </div>
+
+          <form
+            onSubmit={handleSubmit(registerInfo)}
+            className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-zinc-100"
+          >
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                {...register ('name')}
+                  type="text"
+                  name="name"
+                  placeholder="full name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                {...register ('email')}
+                  type="text"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  {...register ('password')}
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-accent">Register</button>
+              </div>
+              <p>
+                Already have an account :
+                <button className="btn text-emerald-500 btn-active btn-link">
+                  <Link to="/login">Login Here</Link>
+                </button>
+              </p>
+              <div
+                // onClick={""}
+                className="flex justify-center items-center cursor-pointer border bg-emerald-200 hover:bg-emerald-300 rounded-full"
+              >
+                <img className="w-12 " src={GoogleIcon} alt="" />
+                <p className="px-4">continue with google</p>
+              </div>
+            </div>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Register;
